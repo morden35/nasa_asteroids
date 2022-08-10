f6aa7bf5a75dfe40f5fdb70eb0945eb378204076
Michelle Orden <morden@uchicago.edu>
2022-08-10 13:03:55 -0400

    Able to install requests. Populating page with for loop.

--- app.py
+++ app.py
@@ -11,13 +11,18 @@ def index():
 
 @app.route('/api/get_objects', methods=['POST'])
 def get_objects():
-    date = json.loads(request.data)
+    date = json.loads(request.data)['date']
+    if date == '':
+        return jsonify({'success': False})
     # print(date)
     # format date 2022-08-01
 
     api_key = '2zcSAHeiiktxliyCHz2eVVzGfUpwPsFqTX97WquF'
     url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={date}&end_date={date}&api_key={api_key}"
     r = requests.get(url)
+    # print(r.json())
+    # try except
     objects = r.json()['near_earth_objects'][date]
+    # print(objects)
 
     return jsonify({'success': True, 'objects': objects})
--- static/script.js
+++ static/script.js
@@ -17,24 +17,74 @@ class NASA extends React.Component {
         .then(data => {
             if (data['success']) {
                 let asteroids = data['objects'];
+                // console.log(asteroids)
                 // first, remove all old asteroids from html
-                let asteroid_ul = document.getElementById("asteroid_list"); // [0]
+                let asteroid_ul = document.getElementById("asteroid_list");
                 while (asteroid_ul.firstChild) {
                     asteroid_ul.removeChild(asteroid_ul.firstChild);
                 }
                 // re-populate page with 'new' asteroids
                 for (let asteroid of asteroids) {
-                    let a = document.createElement("li");
+                    console.log(asteroid)
+                    // create outer list item
+                    let a_li = document.createElement("li");
+                    
+                    // create left column
+                    let a_div_1 = document.createElement("div");
+                    a_div_1.classList.add("col_left");
 
-                    let a_name = document.createTextNode(asteroid['name']);
-                    // let a_diameter_min = document.createTextNode(asteroid['feet']['estimated_diameter_min']);
-                    // let a_diameter_max = document.createTextNode(asteroid['feet']['estimated_diameter_max']);
-                    // let a_miss_dist = document.createTextNode(asteroid['close_approach_data'][0]['miss_distance']['miles']);
-                    // let a_speed = document.createTextNode(asteroid['close_approach_data'][0]['relative_velocity']['miles_per_hour']);
-                    // let a_hazard = document.createTextNode(asteroid['is_potentially_hazardous_asteroid']);
+                    // create titles
+                    let a_h3_1 = document.createElement("h3");
+                    a_h3_1.textContent = "Name:";
+                    let a_h3_2 = document.createElement("h3");
+                    a_h3_2.textContent = "Diameter (ft.):";
+                    let a_h3_3 = document.createElement("h3");
+                    a_h3_3.textContent = "Miss Distance (mi.):";
+                    let a_h3_4 = document.createElement("h3");
+                    a_h3_4.textContent = "Speed (mph):";
+                    let a_h3_5 = document.createElement("h3");
+                    a_h3_5.textContent = "Hazardous:";
 
-                    a.appendChild(a_name);
-                    asteroid_ul.appendChild(a);
+                    // append titles to left column
+                    a_div_1.appendChild(a_h3_1)
+                    a_div_1.appendChild(a_h3_2)
+                    a_div_1.appendChild(a_h3_3)
+                    a_div_1.appendChild(a_h3_4)
+                    a_div_1.appendChild(a_h3_5)
+
+                    // create right column
+                    let a_div_2 = document.createElement("div");
+                    a_div_2.classList.add("col_right");
+
+                    // insert text from api
+                    let a_h3_6 = document.createElement("h3");
+                    a_h3_6.textContent = asteroid['name'];
+                    let a_h3_7 = document.createElement("h3");
+                    a_h3_7.textContent = asteroid['estimated_diameter']['feet']['estimated_diameter_min'] + " - " + asteroid['estimated_diameter']['feet']['estimated_diameter_max'];
+                    let a_h3_8 = document.createElement("h3");
+                    a_h3_8.textContent = asteroid['close_approach_data'][0]['miss_distance']['miles'];
+                    let a_h3_9 = document.createElement("h3");
+                    a_h3_9.textContent = asteroid['close_approach_data'][0]['relative_velocity']['miles_per_hour'];
+                    let a_h3_10 = document.createElement("h3");
+                    a_h3_10.textContent = asteroid['is_potentially_hazardous_asteroid'];
+                    
+                    // append text to right column
+                    a_div_2.appendChild(a_h3_6)
+                    a_div_2.appendChild(a_h3_7)
+                    a_div_2.appendChild(a_h3_8)
+                    a_div_2.appendChild(a_h3_9)
+                    a_div_2.appendChild(a_h3_10)
+
+                    // create horizontal line
+                    let a_hr = document.createElement("hr");
+                    
+                    // append columns and line to list item
+                    a_li.appendChild(a_div_1)
+                    a_li.appendChild(a_div_2)
+                    a_li.appendChild(a_hr)
+
+                    // appemd list item to list
+                    asteroid_ul.appendChild(a_li);
                 }
             }
             else {
